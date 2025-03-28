1dbfb384c709b226c0599a7df50b7660e9cef6b6
Andrew Kulishow <an.kulishou@gmail.com>
2025-03-28 17:31:39 +0100

    make lesson-7

---
+++ iPrody/src/homework/lesson7/Car.java
@@ -0,0 +1,22 @@
+package homework.lesson7;
+
+public class Car {
+
+    private static void startElectricity() {
+
+    }
+
+    private static void startCommand() {
+
+    }
+
+    private static void startFuelSystem() {
+
+    }
+
+    public static void start() {
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+}
